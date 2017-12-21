5ef8a009d560f33b9ace9015622bd37152eb76b8
David <git@djmclaughlin.com>
2017-12-21 16:48:21 -0500

    Setup the tests for the RobotController.Command project.

---
+++ RobotController.Command.Tests/RobotCommandTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RobotController.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotController.Command.Tests
+{
+    [TestClass()]
+    public class RobotCommandTests
+    {
+        [TestMethod()]
+        public void IsDataValid_GoodInput_Test()
+        {
+            int id = 0;
+            string data = "test";
+
+            RobotCommand command = new RobotCommand(id, data);
+
+            Assert.IsNotNull(command.Data);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void IsDataValid_BadInput_Test()
+        {
+            int id = 0;
+            string data = null;
+
+            RobotCommand command = new RobotCommand(id, data);
+        }
+    }
+}
