c593b334d18095e76fcbd84c52c22cc3035bccff
David <git@djmclaughlin.com>
2017-12-21 16:43:45 -0500

    Added a movement command with id 1.

---
+++ RobotController.Command/RobotMoveCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotController.Command
+{
+    public class RobotMoveCommand : RobotCommand
+    {
+        public RobotMoveCommand(string data) : base(RobotMoveCommand.COMMAND_ID, data)
+        {
+        }
+
+        public const int COMMAND_ID = 1;
+    }
+}
