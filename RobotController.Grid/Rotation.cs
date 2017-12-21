64c59980e01d51c638679f1d1c7555157a6b275e
David <git@djmclaughlin.com>
2017-12-20 21:50:19 -0500

    Added a Rotation enum to be used with the Spinner obstruction.

---
+++ RobotController.Grid/Rotation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotController.Grid
+{
+    public enum Rotation
+    {
+        None = 0,
+        CCW90 = -90,
+        CCW180 = -180,
+        CCW270 = -270,
+        CW90 = 90,
+        CW180 = 180,
+        CW270 = 270
+    }
+}
