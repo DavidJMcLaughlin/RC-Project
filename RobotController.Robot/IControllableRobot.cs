0967143d87110c1567aae7179655d9600665c6ef
David <git@djmclaughlin.com>
2017-12-21 20:26:52 -0500

    Setup IControllableRobot interface.

---
+++ RobotController.Robot/IControllableRobot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RobotController.Grid;
+
+namespace RobotController.Robot
+{
+    public interface IControllableRobot
+    {
+        Position GetPosition();
+        Position GetAdvancedPosition();
+
+        void SetPosition(Position position);
+        void Rotate(Rotation rotiation);
+    }
+}
