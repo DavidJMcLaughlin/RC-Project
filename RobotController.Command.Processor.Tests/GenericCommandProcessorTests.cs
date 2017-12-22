3af5072a590f642f46e71b8380b480821242fc12
David <git@djmclaughlin.com>
2017-12-22 09:58:47 -0500

    Started adding the tests for the RobotController.Command.Processor project.

---
+++ RobotController.Command.Processor.Tests/GenericCommandProcessorTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RobotController.Command.Processor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RobotController.Robot;
+
+namespace RobotController.Command.Processor.Tests
+{
+    [TestClass()]
+    public class GenericCommandProcessorTests
+    {
+        private GenericCommandProcessor CommandProcessorToTest = new GenericCommandProcessor();
+
+        [TestMethod()]
+        public void ProcessCommand_Test()
+        {
+            Assert.Fail(); // fail until we have an implementation of IControllableRobot
+
+            RobotMoveCommand command = new RobotMoveCommand("F");
+            IControllableRobot robot = null;
+
+            bool success = false;
+
+            this.CommandProcessorToTest.CommandProcessedSuccessfully += delegate(object sender, RobotCommandEventArgs e)
+            {
+                success = true;
+            };
+
+            this.CommandProcessorToTest.CommandProcessedUnsuccessfully += delegate (object sender, RobotCommandEventArgs e)
+            {
+                Assert.Fail();
+            };
+
+            this.CommandProcessorToTest.ProcessCommand(command, robot);
+
+            Assert.IsTrue(success);
+        }
+
+        [TestMethod()]
+        public void IsCommandValid_GoodInput_Test()
+        {
+            RobotMoveCommand command = new RobotMoveCommand("F");
+
+            ICommandInterpreter ici = new MovementCommandInterpreter();
+
+            this.CommandProcessorToTest.CommandProcessorMap.Add(command.Id, ici);
+
+            bool success = this.CommandProcessorToTest.IsCommandValid(command);
+
+            Assert.IsTrue(success);
+        }
+
+        [TestMethod()]
+        public void IsCommandValid_BadInput_Test()
+        {
+            this.CommandProcessorToTest.CommandProcessorMap.Clear();
+
+            RobotMoveCommand command = new RobotMoveCommand("F");
+
+            bool success = this.CommandProcessorToTest.IsCommandValid(command);
+
+            Assert.IsFalse(success);
+        }
+
+        [TestMethod()]
+        public void TryExecuteCommand_Test()
+        {
+            Assert.Fail(); // fail until we have an implementation of IControllableRobot
+
+
+            RobotMoveCommand command = new RobotMoveCommand("F");
+            ICommandInterpreter ici = new MovementCommandInterpreter();
+            IControllableRobot robot = null;
+
+            this.CommandProcessorToTest.CommandProcessorMap.Add(command.Id, ici);
+
+            bool success = this.CommandProcessorToTest.TryExecuteCommand(command, robot);
+
+            Assert.IsTrue(success);
+        }
+    }
+}
