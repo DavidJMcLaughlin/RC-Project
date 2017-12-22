734c53982f0196d026c6f2911b4721795d20751b
David <git@djmclaughlin.com>
2017-12-22 09:57:36 -0500

    Added an EventArgs extension for RobotCommands.

---
+++ RobotController.Command.Processor/RobotCommandEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotController.Command.Processor
+{
+    public class RobotCommandEventArgs : EventArgs
+    {
+        public RobotCommandEventArgs(RobotCommand command)
+        {
+            this.Command = command;
+            this.Timestamp = DateTime.Now;
+        }
+
+        public RobotCommand Command { get; private set; }
+        /// <summary>
+        /// The DateTime of when the RobotCommandEventArgs object was created.
+        /// </summary>
+        public DateTime Timestamp { get; private set; }
+    }
+}
