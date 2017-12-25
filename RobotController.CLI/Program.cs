92a8f301c057d568211ed93a0eb3ecb7a905ed9f
David <git@djmclaughlin.com>
2017-12-25 13:57:40 -0500

    Another minor graphical change.
    The on screen keypress prompt is now dark gray.

--- RobotController.CLI/Program.cs
+++ RobotController.CLI/Program.cs
@@ -221,6 +221,8 @@ namespace RobotController.CLI
 
         private static void DrawGridStatus()
         {
+            ConsoleColor startingColor = Console.ForegroundColor;
+
             int startingLeftIndex = Console.CursorLeft;
 
             string spacer = "  ";
@@ -232,8 +234,13 @@ namespace RobotController.CLI
             Console.Write(spacer + "Total tiles: {0}", (Program.Grid.Bounds.Width * Program.Grid.Bounds.Height));
             Console.SetCursorPosition(startingLeftIndex, Console.CursorTop + 1);
             Console.Write(spacer + "Total obstructions: {0}", Program.Grid.Obstructions.Count);
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+
             Console.SetCursorPosition(startingLeftIndex, Console.CursorTop + 1);
             Console.Write(spacer + "[1] to regenerate obstructions");
+
+            Console.ForegroundColor = startingColor;
         }
 
         private static void DrawRobotPreviousPositions()
