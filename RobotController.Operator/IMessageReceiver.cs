9d7633648852e6f86364d1827843c4250db7d510
David <git@djmclaughlin.com>
2017-12-24 19:20:31 -0500

    Setup a new project for the robot controller.
    IMessageReceiver.cs
     - An interface for a class that will queue messages and process them at a later time.
    
    RobotOperator.cs:
     - A class for receiving RobotCommands and controlling an IControllableRobot based on those commands.

---
+++ RobotController.Operator/IMessageReceiver.cs
@@ -0,0 +1,15 @@
+﻿using RobotController.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotController.Operator
+{
+    public interface IMessageReceiver
+    {
+        void QueueCommand(RobotCommand command);
+        void RunNextCommand();
+    }
+}
---
+++ RobotController.Operator/RobotOperator.cs
@@ -0,0 +1,70 @@
+﻿using RobotController.Command;
+using RobotController.Command.Processor;
+using RobotController.Robot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotController.Operator
+{
+    /// <summary>
+    /// This object is responsible for receiving RobotCommand objects and directing an IControllableRobot based on their contents
+    /// </summary>
+    public class RobotOperator : IMessageReceiver
+    { 
+        public RobotOperator(IControllableRobot robot)
+        {
+            this.Robot = robot;
+        }
+
+        private Queue<RobotCommand> CommandQueue = new Queue<RobotCommand>();
+        private GenericCommandProcessor CommandProcessor = new GenericCommandProcessor();
+
+        public IControllableRobot Robot { get; private set; }
+
+        public event EventHandler<RobotCommandEventArgs> AcknowledgeCommandReceived;
+
+        /// <summary>
+        /// Stores a command and waits for RunNextCommand() to process it.
+        /// This method triggers the AcknowledgeCommandReceived event.
+        /// </summary>
+        public void QueueCommand(RobotCommand command)
+        {
+            this?.AcknowledgeCommandReceived?.Invoke(this, new RobotCommandEventArgs(command));
+            this.CommandQueue.Enqueue(command);
+        }
+
+        /// <summary>
+        /// Runs the next available command in the Queue
+        /// </summary>
+        public void RunNextCommand()
+        {
+            if (this.CommandQueue != null && this.CommandQueue.Count > 0)
+            {
+                RobotCommand command = this.CommandQueue.Dequeue();
+                this.CommandProcessor.ProcessCommand(command, this.Robot);
+            }
+        }
+
+        /// <summary>
+        /// Loops through all commands in the Queue via RunNextCommand
+        /// </summary>
+        public void RunAllCommands()
+        {
+            while (this.CommandQueue.Count > 0)
+            {
+                this.RunNextCommand();
+            }
+        }
+
+        /// <summary>
+        /// Registeres a ICommandInterpreter to be called to process any command matching a specific command id
+        /// </summary>
+        public void RegisterCommandInterpreter(int id, ICommandInterpreter interpreter)
+        {
+            this.CommandProcessor.CommandProcessorMap.Add(id, interpreter);
+        }
+    }
+}
